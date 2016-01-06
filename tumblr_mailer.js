4e3920b6658cf04bc240e3c673e39858a3ddf30b
Ontima Chung <ontimachung@Ontimas-MacBook-Pro.local>
2016-01-06 11:19:29 -0500

    cleaned up code

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 var ejs = require('ejs'); // loading EJS into our project
 var tumblr = require('tumblr.js');
-
-//var csvFile = fs.readFileSync("friend_list.csv","utf8");
-//console.log(csvFile);
+var client = tumblr.createClient({
+  consumer_key: 'dvepr7TumFmKjuEzGsPSiPi28viWx4CEJCoiK8MGu1v6wYTNjy',
+  consumer_secret: 'cpG0N6Br8doz4exn7pqo0Wu6zrwqjlYpnI9y1TcXmly88DDmOP',
+  token: 'pFnyGzUrFr2CJWFkPADARwdS1ALHWXjxVn7Ac9My22IjESs4iM',
+  token_secret: 'DXWrKlcNv10yv8PuFxhu5ek07kg6fCtyhAblvCnex5Wf9TjwMd'
+});
 
 var fileName = "friend_list.csv";
 
@@ -32,53 +35,23 @@ function csvParse (csvFile) {
 	return objArray;
 }
 
-var cvsData = csvParse(fileName);
-//console.log(cvsData);
-var emailTemplate = fs.readFileSync('email_template.html', 'utf8');
-
-
-//var customizedTemplate = ejs.render(emailTemplate);
-
-var tumblr = require('tumblr.js');
-var client = tumblr.createClient({
-  consumer_key: 'dvepr7TumFmKjuEzGsPSiPi28viWx4CEJCoiK8MGu1v6wYTNjy',
-  consumer_secret: 'cpG0N6Br8doz4exn7pqo0Wu6zrwqjlYpnI9y1TcXmly88DDmOP',
-  token: 'pFnyGzUrFr2CJWFkPADARwdS1ALHWXjxVn7Ac9My22IjESs4iM',
-  token_secret: 'DXWrKlcNv10yv8PuFxhu5ek07kg6fCtyhAblvCnex5Wf9TjwMd'
-});
-
-//client.posts('HOST_NAME_OF_BLOG - ex: myblog.tumblr.com', function(err, blog){
-client.posts('ontima.tumblr.com', function (err, blog){
-  //console.log(blog);
-  var newPosts = []; //to store posts from last 7 days
-  var today = new Date();
-  var posts = blog.posts;
-  for (var i=0; i<blog.total_posts; i++) {
-  	var currPost = posts[i];
-  	//console.log(posts[i].title);
-  	//console.log(posts[i].date);
-  	var dateWritten = new Date(currPost.date);
-  	var numDaysOld = dateDiff(dateWritten, today);
-  	//console.log(numDaysOld);
-  	if (numDaysOld <= 7) {
-  		var postObj = {};
-  		postObj.href = currPost.post_url;
-  		postObj.title = currPost.title;
-  		postObj.date = currPost.date;
-  		newPosts.push(postObj);
-  	}
-  }
-
-  for (var i=0; i<cvsData.length; i++) {
-  	cvsData[i].latestPosts = newPosts;
-  	var customEmail = ejs.render(emailTemplate, cvsData[i]);
-  	console.log(customEmail);
-  }
-
-})
-
-
-
+function getLatestPosts (posts) {
+	var newPosts = []; //to store posts from last 7 days
+	var today = new Date();
+	for (var i=0; i<posts.length; i++) {
+	  	var currPost = posts[i];
+	  	var dateWritten = new Date(currPost.date);
+	  	var numDaysOld = dateDiff(dateWritten, today);
+	  	if (numDaysOld <= 7) {
+	  		var postObj = {};
+	  		postObj.href = currPost.post_url;
+	  		postObj.title = currPost.title;
+	  		postObj.date = currPost.date;
+	  		newPosts.push(postObj);
+	  	}
+	}	
+	return newPosts;
+}
 
 var dateDiff = function( date1, date2 ) {
   //Get 1 day in milliseconds
@@ -94,3 +67,24 @@ var dateDiff = function( date1, date2 ) {
   // Convert back to days and return
   return Math.round(difference_ms/one_day); 
 }
+
+
+////Main program
+
+var cvsData = csvParse(fileName);
+var emailTemplate = fs.readFileSync('email_template.html', 'utf8');
+
+client.posts('ontima.tumblr.com', function (err, blog){
+  var newPosts = getLatestPosts(blog.posts);
+
+  for (var i=0; i<cvsData.length; i++) {
+  	cvsData[i].latestPosts = newPosts;
+  	var customEmail = ejs.render(emailTemplate, cvsData[i]);
+  	console.log(customEmail);
+  }
+
+})
+
+
+
+
